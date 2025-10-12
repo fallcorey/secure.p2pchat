b570f76f56ba2afca6d9a421dbeacead3dd87ab5
fallcorey <fallcorey.a@gmail.com>
2025-10-12 14:23:45 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,13 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("com.google.gms.google-services") version "4.3.10" apply false
-    id("com.google.firebase.crashlytics") version "2.9.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 allprojects {
